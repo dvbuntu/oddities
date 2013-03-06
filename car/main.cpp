28c072e00b7618aa64f0dcca1a60040c17eb9aae
Max <silverhammermba@gmail.com>
2013-03-05 22:39:00 -0500

    Add multiple cars, very basic collision detection
    This code is pretty much hideous, and not DRY, but it works. Take this
    proof of concept and make it awesome.

--- car/main.cpp
+++ car/main.cpp
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <list>
 #include <sstream>
 #include <SFML/Graphics.hpp>
 
@@ -49,8 +50,13 @@ int main()
 
 	sf::Color background(22, 22, 22);
 
-	Car car(view.getCenter());
+	std::list<Car*> cars;
+	cars.push_front(new Car(view.getCenter() - sf::Vector2f(width / 2, 0)));
+	cars.push_front(new Car(view.getCenter()));
 
+	cars.back()->set_gas(1);
+
+	float timescale = 1;
 	sf::Clock clock;
 
 	// game loop
@@ -65,17 +71,8 @@ int main()
 			{
 				switch(event.key.code)
 				{
-					case sf::Keyboard::Left:
-						car.set_gas(0);
-						break;
-					case sf::Keyboard::Right:
-						car.set_gas(1);
-						break;
-					case sf::Keyboard::Up:
-						car.set_brake(0);
-						break;
-					case sf::Keyboard::Down:
-						car.set_brake(1);
+					case sf::Keyboard::Space:
+						timescale = 1 - timescale;
 						break;
 					default:
 						break;
@@ -84,40 +81,81 @@ int main()
 		}
 
 		// frame-time-dependent stuff
-		float time = clock.getElapsedTime().asSeconds();
+		float time = clock.getElapsedTime().asSeconds() * timescale;
 		clock.restart();
 
-		// wrap car around screen
-		if (car.get_pos() > right)
-			car.set_pos(car.get_pos() - width - car.get_size());
+		// wrap cars around screen
+		while (cars.front()->get_pos() > right)
+		{
+			cars.front()->set_pos(cars.front()->get_pos() - width - cars.front()->get_size());
+			cars.push_back(cars.front());
+			cars.pop_front();
+		}
 
-		car.step(time);
+		// update car positions
+		for (auto car : cars)
+			car->step(time);
+
+		// detect collisions
+		auto pcar = cars.rbegin();
+		auto pnext = cars.rbegin();
+		++pnext;
+		for (; pnext != cars.rend(); pcar++, pnext++)
+		{
+			Car& car = **pcar;
+			Car& next = **pnext;
+			if (car.get_rect().intersects(next.get_rect()))
+			{
+				cerr << "Collision at " << (car.get_vel() - next.get_vel()) << " m/s\n";
+				// backup so the cars don't overlap TODO physics!?!?!?!?
+				car.set_pos(next.get_pos() - car.get_size());
+				// pause
+				timescale = 0;
+			}
+		}
+
+		// have to handle last car differently due to wrapping TODO better way?
+		sf::FloatRect back_rect = cars.front()->get_rect();
+		back_rect.left -= width + cars.front()->get_size();
+		if (back_rect.intersects(cars.back()->get_rect()))
+		{
+			cerr << "Collision at " << (cars.front()->get_vel() - cars.back()->get_vel()) << " m/s\n";
+			cars.front()->set_pos(cars.back()->get_pos() + width);
+			timescale = 0;
+		}
 
 		// draw
 		window.clear(background);
 
-		car.draw_on(window);
+		for (auto car : cars)
+			car->draw_on(window);
 
 		// draw car stats
 		window.setView(window.getDefaultView());
 
-		stat_s.str("");
-		stat_s << car.get_vel() << " m/s";
-		stats.setString(stat_s.str());
-		stats.setPosition(10, 10);
-		window.draw(stats);
+		unsigned int i = 0;
+		for (auto pcar = cars.rbegin(); pcar != cars.rend(); pcar++)
+		{
+			const Car& car = **pcar;
+			stat_s.str("");
+			stat_s << car.get_vel() << " m/s";
+			stats.setString(stat_s.str());
+			stats.setPosition(10 + 200 * i, 10);
+			window.draw(stats);
 
-		stat_s.str("");
-		stat_s << car.get_acc() << " m/s^2";
-		stats.setString(stat_s.str());
-		stats.setPosition(10, 22);
-		window.draw(stats);
+			stat_s.str("");
+			stat_s << car.get_acc() << " m/s^2";
+			stats.setString(stat_s.str());
+			stats.setPosition(10 + 200 * i, 22);
+			window.draw(stats);
 
-		stat_s.str("");
-		stat_s << car.get_drag() << " m/s^2";
-		stats.setString(stat_s.str());
-		stats.setPosition(10, 34);
-		window.draw(stats);
+			stat_s.str("");
+			stat_s << car.get_drag() << " m/s^2";
+			stats.setString(stat_s.str());
+			stats.setPosition(10 + 200 * i, 34);
+			window.draw(stats);
+			++i;
+		}
 
 		window.setView(view);
 
