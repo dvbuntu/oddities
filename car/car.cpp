13dae6593290d58fedeb6849ea6ded4f90898b22
vanboxel <the.dvb@gmail.com>
2013-03-22 21:14:36 -0400

    Added very basic self-driving functions and general slowing down based on target velocity.  Not actually used in simulator yet

--- car/car.cpp
+++ car/car.cpp
@@ -17,6 +17,8 @@ Car::Car(const sf::Vector2f& p)
     roll_r = 0.015 * 9.81; // maybe not big enough?
 	drag_m = (0.5 * 2.51516 * 0.45 * 1.225) / mass; // (1/2 * area of front of car * drag coefficient * density of air) / mass
 
+    control = MANUAL;
+
 	rect.setPosition(p);
 	rect.setFillColor(sf::Color(std::rand() % 256, std::rand() % 256, std::rand() % 256));
 }
@@ -33,15 +35,56 @@ void Car::set_brake(float b)
 	brk_acc = b * brk_max;
 }
 
+void Car::set_control(unsigned char c)
+{
+	control = c;
+}
+
+float Car::get_auto_vel(Car leader)
+{
+    float new_vel;
+    /* TODO Put self-driving algorithms into a nice array
+     * algs[control] is the control scheme function.
+     * No changes for MANUAL
+     * Complain if control >= NUM_CONTROL_TYPES
+     * Default to coasting
+     */
+    switch (control)
+    {
+        case MANUAL:
+            new_vel = trg_vel;
+            break;
+        case AUTOMATIC1:
+            new_vel = leader.trg_vel;
+            break;
+        default:
+            new_vel = trg_vel; //Just keep doing what we're doing
+    }
+
+    return new_vel;
+}
+
 void Car::step(float time)
 {
 	acc = 0;
 
-	// if not going as fast as you want to, set acceleration proportional to the difference
+	// if not going the speed you want to, set acceleration proportional to the difference
+    // TODO Change to accept an acc and compute those before step
 	if (vel < trg_vel)
+    {
 		acc = ((max_vel - vel) / (float)max_vel) * max_acc;
+    }
+    /* slow down in proportion to trg_vel
+     * Careful with epsilon so we don't divide by zero
+     */
+    else if (vel > trg_vel)
+    {
+		acc = ((trg_vel - vel) / ( (float)trg_vel + 
+               FLT_EPSILON)) * brk_acc;
+    }
 
-	if (brk_acc < 0)
+    // if set to fixed braking or targeting low vel, ignore the trg_vel
+	if (brk_acc < 0 || trg_vel < FLT_EPSILON)
 		acc = brk_acc;
 
 	// deceleration from drag
--- car/car.hpp
+++ car/car.hpp
@@ -8,7 +8,22 @@
 #define CAR_HPP
 
 #include <cstdlib>
+#include <cfloat>
 #include <SFML/Graphics.hpp>
+
+// define manual and automatic control constants
+#ifndef MANUAL
+#define MANUAL 0
+#endif
+
+#ifndef AUTOMATIC1
+#define AUTOMATIC1 1
+#endif
+
+#ifndef NUM_CONTROL_TYPES
+#define NUM_CONTROL_TYPES 2 // hard-coded, easy way to automate?
+#endif
+
 
 class Car
 {
@@ -24,8 +39,10 @@ class Car
 	float mass; // mass of vehicle in kg
 	float weight; // weight of vehicle in mg * m / s^2
 	float roll_r; // deceleration due to rolling resistance, constant for a given vehicle/tire types/mass
-
 	float drag;
+
+    unsigned char control; // manual, or type of self-driving
+
 	sf::RectangleShape rect;
 public:
 	Car(const sf::Vector2f& p);
@@ -69,6 +86,11 @@ public:
 	void set_gas(float g);
 	// set brake pedal amount between 0 and 1
 	void set_brake(float b);
+	// set control manual or automatic
+	void set_control(unsigned char c);
+
+    // get the automated velocity based on leading car
+    float get_auto_vel(Car leader);
 
 	// advance simulation
 	void step(float time);
