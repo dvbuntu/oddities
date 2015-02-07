1af124db7338b5b6e8818dc5b6430533b6b0b8a1
Dan Van Boxel <the.dvb@gmail.com>
2015-02-07 07:45:51 -0500

    More sage computations

--- stopping_dist.py
+++ stopping_dist.py
@@ -15,14 +15,36 @@ follow_accx = acc1 * t**2 / 2 + v1*t + x1
 h0 = leadx - follow_accx 
 
 # front of following car when starting to slow down
-follow_tr1_x = follow_accx 
+follow_tr1_x = follow_accx.substitute(t=tr1) 
 
 # velocity of following car when starting to slow down
-
+follow_tr1_v = (acc1*tr1 + v1)
 
 # front of following car once starting to slow down
-
+follow_decx = dec1 * (t-tr1)**2 / 2 + follow_tr1_v * (t-tr1) + follow_tr1_v
 
 # tb <= t <= t_stopl, both cars slowing down
-h1 = leadx - (dec1*(t-tr1)^2 /2 +  (acc1 * t^2 / 2 + v1*t + x1))
+h1 = leadx - follow_decx
 
+# time of lead car stopping
+ts0 = v0 / dec0
+
+# back of lead car after stopping, final stopping position
+x0_fin = leadx.substitute(t=ts0)
+
+# following car velocity
+v1_dec = follow_tr1_v + dec1 * (t-tr1)
+
+# time when following car stops
+ts1 = v1_dec.solve(t)[0].right()
+
+# front of the following car when stopped
+x1_fin = follow_decx.substitute(t=ts1)
+
+# may set up two x positions and solve for when they are equal??
+# or solve for t, compare to tr1 (and likewise for other)
+# if h0 <= 0 before t = tr1, crashed before we had a chance to stop
+
+# if h1 <= 0 with time between tr1 and ts1, crashed before leader stops
+
+# if x1_fin >= x0_fin, crash after leader stops
